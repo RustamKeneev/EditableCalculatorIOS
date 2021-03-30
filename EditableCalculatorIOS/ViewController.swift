dc63858126be147979786fc4e0d2129ae4240afd
rustamkeneev <dr.rustamkeneev@gmail.com>
2021-03-31 01:32:35 +0600

    update project

--- EditableCalculatorIOS/ViewController.swift
+++ EditableCalculatorIOS/ViewController.swift
@@ -230,15 +230,21 @@ class ViewController: UIViewController {
     }
     
     @objc func clickCosinus(view:UIButton){
-        
+//        let numberOne: Double = Double(fieldOne.text ?? "0") ?? 0
+//        let numberTwo: Double = Double(fieldTwo.text ?? "0") ?? 0
+//        resultLabel.text = String((numberOne * Double.pi) /  numberTwo)
     }
     
     @objc func clickRootSquared (view: UIButton){
-        
+        let numberOne: Double = Double(fieldOne.text ?? "0") ?? 0
+        let numberTwo: Double = Double(fieldTwo.text ?? "0") ?? 0
+        resultLabel.text = String(numberOne.squareRoot())
     }
     
     @objc func clickTangels(view: UIButton){
-        
+//        let numberOne: Double = Double(fieldOne.text ?? "0") ?? 0
+//        let numberTwo: Double = Double(fieldTwo.text ?? "0") ?? 0
+//            resultLabel.text = String(numberOne.squareRoot())
     }
 }
 
